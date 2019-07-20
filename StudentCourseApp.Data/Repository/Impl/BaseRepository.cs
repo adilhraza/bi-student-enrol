076890e40f1403237d2a432a6e21b2124941008e
Adil H Raza <ecoderie@gmail.com>
2019-07-20 23:10:04 +0100

    Scaffolding - Data Layer - adding base repository

---
+++ StudentCourseApp.Data/Repository/Impl/BaseRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace StudentCourseApp.Data.Repository.Impl
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly DbContext Context;
+
+        public BaseRepository(DbContext context)
+        {
+            Context = context;
+        }
+
+        public void Add(T entity)
+        {
+            Context.Set<T>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            Context.Set<T>().AddRange(entities);
+        }
+
+        public void Remove(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            Context.Set<T>().RemoveRange(entities);
+        }
+
+        public T Get(int id)
+        {
+            return Context.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>().ToList();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicatExpression)
+        {
+            return Context.Set<T>().Where(predicatExpression);
+        }
+    }
+}
