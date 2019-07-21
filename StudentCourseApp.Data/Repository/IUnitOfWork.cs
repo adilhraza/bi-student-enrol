5ce176352e716687c2f140c107c38e886d3535c8
Adil H Raza <ecoderie@gmail.com>
2019-07-21 22:54:04 +0100

    Attempted to add unit of work pattern, Problems: 1. Need to figure out a way to use DI with UOW. currently UOW class is responsible for initializing all repositories. Thsi does not look right. 2. We are defining the new repository in AppDbContext class and then also in UOW class, this is duplication, there has to be a better way to do things. 3. The following links seem useful discussion: https://stackoverflow.com/questions/16064902/dependency-injection-in-unit-of-work-pattern-using-repositories https://www.programmingwithwolfgang.com/repository-and-unit-of-work-pattern/

---
+++ StudentCourseApp.Data/Repository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StudentCourseApp.Data.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> Complete();
+    }
+}
---
+++ StudentCourseApp.Data/Repository/Impl/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using StudentCourseApp.Data.Infrastructure;
+
+namespace StudentCourseApp.Data.Repository.Impl
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Complete()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
