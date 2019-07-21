c07d52d9d1b5147741d190c9e71f8bcf7df91637
Adil H Raza <ecoderie@gmail.com>
2019-07-21 23:56:16 +0100

    Scaffolding - Data Layer - adding base repository

---
+++ StudentCourseApp.Data/Repository/IBaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace StudentCourseApp.Data.Repository
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        void Add(T entity);
+
+        void AddRange(IEnumerable<T> entities);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+
+        void Update(T entity);
+
+        void Save();
+
+        T Get(int id);
+
+        IEnumerable<T> GetAll();
+
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicatExpression);
+    }
+}
---
+++ StudentCourseApp.Data/Repository/Impl/BaseRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using StudentCourseApp.Data.Infrastructure;
+
+namespace StudentCourseApp.Data.Repository.Impl
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly AppDbContext _dbContext;
+        private DbSet<T> _dbSet;
+
+        public BaseRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _dbSet.AddRange(entities);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public T Get(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicatExpression)
+        {
+            return _dbSet.Where(predicatExpression);
+        }
+    }
+}
