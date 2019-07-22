b8bf44d42f975b3ce51c623abc6b4433388ec7c2
Adil H Raza <ecoderie@gmail.com>
2019-07-22 16:22:25 +0100

    Course and Student entity to model mappers added

---
+++ StudentCourseApp.Services/Mappers/CourseMapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using StudentCourseApp.Data.Entity;
+using StudentCourseApp.Shared.Models;
+
+namespace StudentCourseApp.Services.Mappers
+{
+    public static class CourseMapper
+    {
+        public static CourseModel ToModel(this Course entity)
+        {
+            return new CourseModel
+            {
+                Students = entity.Students.ToModels(),
+                CourseCode = entity.CourseCode,
+                Id = entity.CourseId,
+                CourseName = entity.CourseName,
+                EndDate = entity.EndDate,
+                StartDate = entity.StartDate,
+                TeacherName = entity.TeacherName
+            };
+        }
+
+        public static List<CourseModel> ToModels(this IEnumerable<Course> entities)
+        {
+            return entities.Select(ToModel).ToList();
+        }
+    }
+}
---
+++ StudentCourseApp.Services/Mappers/StudentMapper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using StudentCourseApp.Data.Entity;
+using StudentCourseApp.Shared.Models;
+
+namespace StudentCourseApp.Services.Mappers
+{
+    public static class StudentMapper
+    {
+        public static StudentModel ToModel(this Student entity)
+        {
+            return new StudentModel
+            {
+                Id = entity.StudentId,
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                Gender = entity.Gender,
+                DateOfBirth = entity.DateOfBirth,
+                AddressLine1 = entity.AddressLine1,
+                AddressLine2 = entity.AddressLine2,
+                AddressLine3 = entity.AddressLine3,
+                Courses = entity.Courses.ToModels()
+            };
+        }
+
+        public static List<StudentModel> ToModels(this IEnumerable<Student> entities)
+        {
+            return entities.Select(ToModel).ToList();
+        }
+    }
+}
