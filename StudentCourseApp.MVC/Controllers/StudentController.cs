d04cbd4dadf0ccf74bc31460169f45d7bb03ebce
Adil H Raza <ecoderie@gmail.com>
2019-07-22 16:29:04 +0100

    Student controller with actions implemented.

---
+++ StudentCourseApp.MVC/Controllers/StudentController.cs
@@ -0,0 +1,115 @@
+﻿using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using StudentCourseApp.Services;
+using StudentCourseApp.Shared.Models;
+
+namespace StudentCourseApp.MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(_studentService.GetAll().ToList());
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var studentModel = _studentService.Get(id.Value);
+            if (studentModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentModel);
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Gender,DateOfBirth,AddressLine1,AddressLine2,AddressLine3")] StudentModel studentModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _studentService.AddNew(studentModel);
+                return RedirectToAction("Index");
+            }
+
+            return View(studentModel);
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var studentModel = _studentService.Get(id.Value);
+            if (studentModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentModel);
+        }
+
+        // POST: Student/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Gender,DateOfBirth,AddressLine1,AddressLine2,AddressLine3")] StudentModel studentModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _studentService.UpdateStudent(studentModel);
+                return RedirectToAction("Index");
+            }
+            return View(studentModel);
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var studentModel = _studentService.Get(id.Value);
+            if (studentModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentModel);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _studentService.RemoveStudent(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
