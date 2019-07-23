61de52a38ec6b50da94e6d5f3d5f695d631d0377
Adil H Raza <ecoderie@gmail.com>
2019-07-23 23:37:11 +0100

    Tidying up un-used code.

--- StudentCourseApp.MVC/Controllers/HomeController.cs
+++ StudentCourseApp.MVC/Controllers/HomeController.cs
@@ -1,36 +1,10 @@
 ﻿using System.Web.Mvc;
-using StudentCourseApp.Services;
 
 namespace StudentCourseApp.MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IStudentService _studentService;
-
-        public HomeController(IStudentService studentService)
-        {
-            _studentService = studentService;
-        }
-
         public ActionResult Index()
-        {
-            return View();
-        }
-
-        [Authorize(Roles = "Student")]
-        public ActionResult Student()
-        {
-            return View();
-        }
-
-        [Authorize(Roles = "Admin")]
-        public ActionResult Students()
-        {
-            return View();
-        }
-
-        [Authorize(Roles = "Admin")]
-        public ActionResult Courses()
         {
             return View();
         }
